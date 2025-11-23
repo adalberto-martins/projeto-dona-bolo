3f13a45d47e5aa2895191f3d89dbe633164aa986
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-11-23 15:39:56 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// MENU RESPONSIVO
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
